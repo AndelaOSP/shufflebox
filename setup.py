754825b32c380a1239bef6afd38c2ce42d3b573f
Jee Githinji Gikera <githinjigikera@gmail.com>
2017-01-31 12:37:48 +0300

    (chore) edit setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,22 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
+#-*- coding: utf-8 -*-
 
 from setuptools import setup, find_packages
 
 with open('README.md') as f:
     readme = f.read()
 
-# with open('LICENSE') as f:
-#     license = f.read()
+with open('LICENSE') as f:
+    license = f.read()
 
 setup(
     name='shufflebox',
     version='0.1.0',
-    description='Simple randomizer for shuffling.',
+    description='A randomizer package for shuffling.',
     long_description=readme,
     author='Andela Kenya',
-    author_email='herbert.kagumba@andela.com',
+    author_email='herbert.kagumba@andela.com, githinji.gikera@andela.com',
     url='https://github.com/AndelaOSP/shufflebox',
-    # license=license,
+    license=license,
     packages=find_packages(exclude=('tests', 'docs'))
 )
