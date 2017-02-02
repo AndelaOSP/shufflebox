156a7484e1f27fae35064eb3dd3e2d303135feb7
Herbert Kagumba <herbert.kagumba@andela.com>
2017-02-02 15:59:53 +0300

    (feat): Make package pip installable

--- setup.py
+++ setup.py
@@ -1,22 +1,40 @@
-#!/usr/bin/env python
-#-*- coding: utf-8 -*-
-
 from setuptools import setup, find_packages
+from codecs import open
+from os import path
 
-with open('README.md') as f:
-    readme = f.read()
+__version__ = '0.0.1'
 
-with open('LICENSE') as f:
-    license = f.read()
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+# get the dependencies and installs
+with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
+    all_reqs = f.read().split('\n')
+
+install_requires = [x.strip() for x in all_reqs if 'git+' not in x]
+dependency_links = [x.strip().replace('git+', '') for x in all_reqs if x.startswith('git+')]
 
 setup(
     name='shufflebox',
-    version='0.1.0',
+    version=__version__,
     description='A randomizer package for shuffling.',
-    long_description=readme,
-    author='Andela Kenya',
-    author_email='herbert.kagumba@andela.com, githinji.gikera@andela.com',
+    long_description=long_description,
     url='https://github.com/AndelaOSP/shufflebox',
-    license=license,
-    packages=find_packages(exclude=('tests', 'docs'))
+    download_url='https://github.com/AndelaOSP/shufflebox/tarball/' + __version__,
+    license='BSD',
+    classifiers=[
+      'Development Status :: 3 - Alpha',
+      'Intended Audience :: Developers',
+      'Programming Language :: Python :: 3',
+    ],
+    keywords='',
+    packages=find_packages(exclude=['docs', 'tests*']),
+    include_package_data=True,
+    author='Andela Kenya',
+    install_requires=install_requires,
+    dependency_links=dependency_links,
+    author_email='herbert.kagumba@andela.com, githinji.gikera@andela.com'
 )
